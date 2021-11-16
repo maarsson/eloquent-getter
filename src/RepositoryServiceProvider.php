e4013a257cc14055c6e766cbf6d79e6a8423aaa0
VMaarsson <viktor@maarsson.se>
2021-11-16 19:51:45 +0100

    Create package service provider

---
+++ src/RepositoryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Maarsson\Repository;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
