ab6c697356201a2d8f5fe7ba8a3b28a547f21ac7
VMaarsson <viktor@maarsson.se>
2021-11-17 09:41:02 +0100

    Add repository interface

---
+++ src/Interfaces/EloquentRepositoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Maarsson\Repository\Interfaces;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+/**
+ * This interface describes an eloquent repository interface.
+ */
+interface EloquentRepositoryInterface
+{
+    /**
+     * Creates a new entity.
+     *
+     * @param array $attributes
+     *
+     * @return Illuminate\Database\Eloquent\Model
+     */
+    public function create(array $attributes): Model;
+
+    /**
+     * Finds an entity by its ID.
+     *
+     * @param mixed $id
+     *
+     * @return Illuminate\Database\Eloquent\Model|Illuminate\Support\Collection
+     */
+    public function find($id): ?Model;
+}
