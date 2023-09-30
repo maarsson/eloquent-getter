96a7b9141cf58d51f356e2514e61fd53209ab45a
VMaarsson <viktor@maarsson.se>
2023-09-30 19:24:41 +0200

    Unite traits

--- src/Traits/GetterableTrait.php
+++ src/Traits/GetterableTrait.php
@@ -2,10 +2,38 @@
 
 namespace Maarsson\Repository\Traits;
 
+use Illuminate\Support\Str;
 use Maarsson\Repository\Getters\AbstractEloquentGetter;
 
 trait GetterableTrait
 {
+    /**
+     * The required model attributes and/or relations´ attributes,
+     * even with column specifications.
+     * Note that the subrelations´ id must be given when querying subrelation.
+     *
+     * @var array<string>
+     */
+    private array $withAttributes = [];
+
+    /**
+     * Get a subset of the model's attributes and/or relations' attributes.
+     *
+     * @param array $attributes
+     *
+     * @return array
+     */
+    public function withAttributes(?array $attributes = []): array
+    {
+        $this->withAttributes = $attributes;
+
+        return empty($this->withAttributes) ?
+            $this->toArray()
+            :
+            $this->load($this->getValidRelations())
+                ->only($this->getValidAttributes());
+    }
+
     /**
      * Scope a query to apply given filter.
      *
@@ -28,5 +56,33 @@ trait GetterableTrait
     public function order(?AbstractEloquentGetter $getter = null): AbstractEloquentGetter
     {
         return $getter->apply($this->model()->query())->order();
+    }
+
+    /**
+     * Filters the required attributes
+     * to only the valid relations.
+     *
+     * @return array
+     */
+    private function getValidRelations(): array
+    {
+        return collect($this->withAttributes)->filter(function ($attribute) {
+            $withoutColumnSpecifications = explode(':', $attribute)[0];
+
+            return $this->isRelation($withoutColumnSpecifications) || Str::contains($withoutColumnSpecifications, '.');
+        })->toArray();
+    }
+
+    /**
+     * Filters the required attributes
+     * to only the valid attributes.
+     *
+     * @return array
+     */
+    private function getValidAttributes(): array
+    {
+        return collect($this->withAttributes)->map(function ($attribute) {
+            return explode('.', explode(':', $attribute)[0])[0];
+        })->toArray();
     }
 }
